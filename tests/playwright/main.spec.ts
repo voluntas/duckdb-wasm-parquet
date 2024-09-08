729b6942b358427c1524fdb384eb401c9cbce9f8
voluntas <nakai@shiguredo.jp>
2024-09-08 20:45:36 +0900

    CI のベースを追加

--- tests/playwright/main.spec.ts
+++ tests/playwright/main.spec.ts
@@ -2,5 +2,18 @@ import { expect, test } from '@playwright/test'
 
 test('scan parquet', async ({ page }) => {
   await page.goto('http://localhost:5173/index.html')
+  // 初期値が0であることを確認
+  await expect(page.locator('#scanned')).toHaveText('Scanned: false')
+
+  // 「Scan Parquet」ボタンをクリック
   await page.click('#scan-parquet')
+
+  // true になるまで待機（最大10秒）
+  await expect(page.locator('#scanned')).toHaveText('Scanned: true', { timeout: 30000 })
+
+  // 「load table」ボタンをクリック
+  await page.click('#load-table')
+
+  // テーブルが読み込まれたことを確認
+  await expect(page.locator('#table')).toBeVisible()
 })
